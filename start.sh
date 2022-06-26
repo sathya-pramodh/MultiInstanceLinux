ad88e921848e5c17004c7c9df93826fc59a21ce1
Sathya Pramodh <sathyapramodh17@gmail.com>
2022-06-26 14:09:20 +0530

    Removed bug in start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 WORKING_DIR=$(pwd)
 CONFIG=$WORKING_DIR/config.py
 if ! [ -f "$CONFIG" ]; then
-	cp $WORKING_DIR/default_config.py $FILE
+	cp $WORKING_DIR/default_config.py $CONFIG
 fi
 sudo python3 multi_instance.py
