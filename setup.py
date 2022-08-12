b98c0d0909289d3fd15db7100ec95493399f9bf6
Sathya Pramodh <sathyapramodh17@gmail.com>
2022-08-13 01:54:46 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_long_description():
 
 setup(
     name="MultiInstanceLinux",
-    version="1.3.4",
+    version="1.3.5",
     author="sathya-pramodh",
     author_email="sathyapramodh17@gmail.com",
     description="Multi Instance Macro Handler for Minecraft on Linux.",
